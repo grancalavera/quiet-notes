7e8963b786cc784685eb208d2ac485cdad2f3def
Leon Coto <leoncoto@gmail.com>
2021-06-13 15:57:10 +0100

    configure firebase app

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,14 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
-firebase.initializeApp({});
+firebase.initializeApp({
+  apiKey: "AIzaSyB-1sXAYdvXVOQgHUcZwP9CpI6dNq1wi7Y",
+  authDomain: "quiet-notes-e83fb.firebaseapp.com",
+  projectId: "quiet-notes-e83fb",
+  storageBucket: "quiet-notes-e83fb.appspot.com",
+  messagingSenderId: "730652202246",
+  appId: "1:730652202246:web:a4b7f52a8b4ae087e9ba87",
+});
 
 export const App = () => <>Quiet Notes</>;
 
