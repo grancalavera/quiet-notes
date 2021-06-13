049b6cbe7f0e128a97af7cfd72b648aea92d2a85
Leon Coto <leoncoto@gmail.com>
2021-06-13 15:53:39 +0100

    firebase setup

---
+++ src/app.tsx
@@ -0,0 +1,20 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+firebase.initializeApp({});
+
+export const App = () => <>Quiet Notes</>;
+
+/*
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="/__/firebase/8.6.7/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+
+<!-- Initialize Firebase -->
+<script src="/__/firebase/init.js"></script>f
+
+9099
+*/
--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
 import "./index.scss";
 import { Toolbar } from "./toolbar/toolbar";
+import { App } from "./app";
 
 ReactDOM.render(
   <React.StrictMode>
     <Toolbar />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
