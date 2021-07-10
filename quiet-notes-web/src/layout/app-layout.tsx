5ea5e1ea6b173b3320b3474d0753891312fdaf8b
Leon Coto <leoncoto@gmail.com>
2021-07-10 11:30:37 +0100

    fix scrollbar on sidebar

--- quiet-notes-web/src/layout/app-layout.tsx
+++ quiet-notes-web/src/layout/app-layout.tsx
@@ -15,20 +15,17 @@ interface AppLayoutProps {
 }
 
 const b = block("app-layout");
-const cell = b("cell").toString();
-const left = b("left").toString();
-const right = b("right").toString();
 
 export const AppLayout = (props: AppLayoutProps) => {
   return (
     <div className={b({}).mix(props.className)}>
-      <div className={b("header").mix(cell)}>{props.header}</div>
+      <div className={b("header")}>{props.header}</div>
 
-      <div className={b("sidebar-toolbar").mix(cell, left)}>{props.sidebarToolbar}</div>
-      <div className={b("sidebar").mix(cell, left)}>{props.sidebar}</div>
+      <div className={b("sidebar-toolbar")}>{props.sidebarToolbar}</div>
+      <div className={b("sidebar")}>{props.sidebar}</div>
 
-      <div className={b("editor-toolbar").mix(cell, right)}>{props.editorToolbar}</div>
-      <div className={b("editor").mix(cell, right)}>{props.editor}</div>
+      <div className={b("editor-toolbar")}>{props.editorToolbar}</div>
+      <div className={b("editor")}>{props.editor}</div>
     </div>
   );
 };
