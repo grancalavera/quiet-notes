1ce2ced7ebcb616ecf6780c4cb5fe72fedc738e7
Leon Coto <leoncoto@gmail.com>
2021-07-31 20:13:47 +0100

    promote default admin to admin on account creation

--- quiet-notes-functions/src/index.ts
+++ quiet-notes-functions/src/index.ts
@@ -1,17 +1,24 @@
 import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
+import { UserRecord } from "firebase-functions/lib/providers/auth";
 
 admin.initializeApp({
   credential: admin.credential.applicationDefault(),
 });
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
-
-const config = functions.config().quiet_notes;
-console.log({ config });
-
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
+export const setDefaultAdmin = functions.auth.user().onCreate(async (user) => {
+  if (user.email === functions.config().quiet_notes.default_admin) {
+    return grantAdminRole(user);
+  }
 });
+
+const grantAdminRole = async (user: UserRecord): Promise<void> => {
+  const roles = user.customClaims?.roles ?? [];
+
+  if (!roles.includes("admin")) {
+    console.log("grant admin role to", { user });
+    return admin.auth().setCustomUserClaims(user.uid, { roles: [...roles, "admin"] });
+  } else {
+    console.log("already an admin, ignore", { user });
+  }
+};
