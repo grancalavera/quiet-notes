c95768a20e35e8a9fa262598f53e118b70f7063e
Leon Coto <leoncoto@gmail.com>
2021-07-31 19:28:03 +0100

    setup admin sdk + read app config

--- quiet-notes-functions/src/index.ts
+++ quiet-notes-functions/src/index.ts
@@ -1,7 +1,15 @@
 import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+});
 
 // Start writing Firebase Functions
 // https://firebase.google.com/docs/functions/typescript
+
+const config = functions.config().quiet_notes;
+console.log({ config });
 
 export const helloWorld = functions.https.onRequest((request, response) => {
   functions.logger.info("Hello logs!", { structuredData: true });
