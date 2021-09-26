c912acc33cced05c331e5384fc3b8e89e3ba4331
Leon Coto <leoncoto@gmail.com>
2021-09-26 16:48:02 +0100

    add scripts to start app at top level

--- quiet-notes-tools/src/setup.ts
+++ quiet-notes-tools/src/setup.ts
@@ -1,32 +1,33 @@
 import { resolve } from "path";
-import { readFileSync } from "fs";
+import { readFileSync, writeFileSync } from "fs";
 
-interface EnvLocal {
-  authPort: number;
-  firestorePort: number;
-  functionsPort: number;
-}
+type EmulatorPorts = Record<"auth" | "firestore" | "functions", number>;
 
-const envLocal = ({ authPort, firestorePort, functionsPort }: EnvLocal) => `
-REACT_APP_FIREBASE_EMULATOR_AUTH=http://localhost:${authPort}
-REACT_APP_FIREBASE_EMULATOR_FIRESTORE_PORT=${firestorePort}
-REACT_APP_FIREBASE_EMULATOR_FUNCTIONS_PORT=${functionsPort}
+const getEnvLocal = (ports: EmulatorPorts) => `
+REACT_APP_FIREBASE_EMULATOR_AUTH=http://localhost:${ports.auth}
+REACT_APP_FIREBASE_EMULATOR_FIRESTORE_PORT=${ports.firestore}
+REACT_APP_FIREBASE_EMULATOR_FUNCTIONS_PORT=${ports.functions}
 BROWSER=none
 `;
 
-const getPorts = (config: any): EnvLocal => {
+const getEmulatorPorts = (config: any): EmulatorPorts => {
   try {
     return {
-      authPort: config.emulators.auth.port,
-      firestorePort: config.emulators.firestore.port,
-      functionsPort: config.emulators.functions.port,
+      auth: config.emulators.auth.port,
+      firestore: config.emulators.firestore.port,
+      functions: config.emulators.functions.port,
     };
   } catch {
     console.error("failed to read ports from firebase.json, using defaults");
-    return { authPort: 9099, firestorePort: 8080, functionsPort: 5001 };
+    return { auth: 9099, firestore: 8080, functions: 5001 };
   }
 };
 
-const path = resolve(__dirname, "../../firebase.json");
-const firebaseJson = JSON.parse(readFileSync(path, "utf8"));
-console.log(envLocal(getPorts(firebaseJson)));
+const firebaseConfigPath = resolve(__dirname, "../../firebase.json");
+const envLocalPath = resolve(__dirname, "../../quiet-notes-web/.env.local");
+
+const firebaseConfig = JSON.parse(readFileSync(firebaseConfigPath, "utf8"));
+const emulatorPorts = getEmulatorPorts(firebaseConfig);
+const envLocal = getEnvLocal(emulatorPorts);
+
+writeFileSync(envLocalPath, envLocal, { encoding: "utf8" });
