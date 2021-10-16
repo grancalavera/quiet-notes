2b7fa0e0f64a119527fe027afe75df22e81d384e
Leon Coto <leoncoto@gmail.com>
2021-10-16 16:57:26 +0100

    repurpose toolbars

--- quiet-notes-web/src/notebook/notebook-toolbars.tsx
+++ quiet-notes-web/src/notebook/notebook-toolbars.tsx
@@ -1,25 +1,18 @@
 import { block } from "../app/bem";
 import { CreateNoteButton } from "../components/CreateNoteButton";
-import { DeleteNoteButton } from "../components/DeleteNoteButton";
-import { useSelectedNoteId } from "./notebook-state";
 import "./notebook-toolbars.scss";
 import { SortMenu } from "./SortNotesMenu";
 
 const b = block("note-editor-toolbar");
 
-export const NoteEditorToolbar = () => {
-  const selectedNoteId = useSelectedNoteId();
-
-  return (
-    <div className={b()}>
-      {!!selectedNoteId && <DeleteNoteButton noteId={selectedNoteId} isSelected />}
-    </div>
-  );
-};
+export const NoteEditorToolbar = () => (
+  <div className={b()}>
+    <CreateNoteButton />
+  </div>
+);
 
 export const SidebarToolbar = () => (
   <div className={b()}>
     <SortMenu />
-    <CreateNoteButton />
   </div>
 );
