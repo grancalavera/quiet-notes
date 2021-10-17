c1b5b176769a001df5c06471387a7beb36dc6153
Leon Coto <leoncoto@gmail.com>
2021-10-17 18:57:17 +0100

    tidy up

--- quiet-notes-web/src/app/app-header.tsx
+++ quiet-notes-web/src/app/app-header.tsx
@@ -100,38 +100,3 @@ const ToggleThemeButton = () => {
   const toggleTheme = useToggleQNTheme();
   return <ToggleThemeSwitch checked={theme === "dark"} onChange={() => toggleTheme()} />;
 };
-
-export default function BasicPopover() {
-  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
-
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  const open = Boolean(anchorEl);
-  const id = open ? "simple-popover" : undefined;
-
-  return (
-    <div>
-      <Button aria-describedby={id} variant="contained" onClick={handleClick}>
-        Open Popover
-      </Button>
-      <Popover
-        id={id}
-        open={open}
-        anchorEl={anchorEl}
-        onClose={handleClose}
-        anchorOrigin={{
-          vertical: "bottom",
-          horizontal: "left",
-        }}
-      >
-        <Typography sx={{ p: 2 }}>The content of the Popover.</Typography>
-      </Popover>
-    </div>
-  );
-}
