ca6f872ef11d8dd39afa44182e17afd81dc2027c
Leon Coto <leoncoto@gmail.com>
2021-12-31 14:53:27 +0000

    remove unused api

--- quiet-notes-web/src/notebook/notebook-create-note-handlers.tsx
+++ quiet-notes-web/src/notebook/notebook-create-note-handlers.tsx
@@ -1,6 +1,6 @@
 import { VFC } from "react";
 import { Redirect } from "react-router-dom";
-import { useCreatedNoteId, useDeletedNoteId } from "./notebook-state";
+import { useCreatedNoteId } from "./notebook-state";
 
 export const CreatedNoteHandler: VFC = () => {
   const noteId = useCreatedNoteId();
