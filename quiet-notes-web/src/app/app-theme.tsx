1adcdf5db24c22ec59560d70f787d8e764c5287f
Leon Coto <leoncoto@gmail.com>
2022-01-15 16:29:11 +0000

    fix color theme for TextAreaAutosize

--- quiet-notes-web/src/app/app-theme.tsx
+++ quiet-notes-web/src/app/app-theme.tsx
@@ -1,3 +1,4 @@
+import CssBaseline from "@mui/material/CssBaseline";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { FC, useMemo } from "react";
 import { useAppTheme } from "./use-app-theme";
@@ -5,5 +6,10 @@ import { useAppTheme } from "./use-app-theme";
 export const AppTheme: FC = ({ children }) => {
   const mode = useAppTheme();
   const theme = useMemo(() => createTheme({ palette: { mode } }), [mode]);
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {children}
+    </ThemeProvider>
+  );
 };
