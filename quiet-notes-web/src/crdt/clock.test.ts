5ee9f973037434ff5a7a6a7b22a3c66d9b9073d7
Leon Coto <leoncoto@gmail.com>
2022-04-03 17:29:19 +0100

    benchmark different vector clock implementations

--- quiet-notes-web/src/crdt/clock.test.ts
+++ quiet-notes-web/src/crdt/clock.test.ts
@@ -1,29 +1,44 @@
 import {
-  compareClocks_internal,
-  isConcurrent,
-  isEqual,
-  isLessThan,
-  isLessThanOrEqual,
-  orderTimes_internal,
+  clockOrderDeclarative,
+  clockOrderImperative,
+  IsClockOrder,
+  clockOrderImperativeInlined,
+  clockOrderImperativeForEach,
+  clockOrderZ,
 } from "./clock";
 
-describe("public api", () => {
+interface Scenario {
+  name: string;
+  compare: IsClockOrder;
+}
+
+const scenarios: Scenario[] = [
+  { name: "declarative", compare: clockOrderDeclarative },
+  { name: "imperative", compare: clockOrderImperative },
+  { name: "imperative inlined", compare: clockOrderImperativeInlined },
+  { name: "imperative for each", compare: clockOrderImperativeForEach },
+  { name: "z", compare: clockOrderZ },
+];
+
+describe.each(scenarios)("Public API: $name", ({ compare }) => {
+  const { isConcurrent, isEqual, isLessThan, isLessThanOrEqual } = compare;
+
   it("empty clocks", () => {
     const actual = isEqual({}, {});
     expect(actual).toBe(true);
   });
 
-  it("EQ", () => {
+  it("Equal", () => {
     const actual = isEqual({ x: 0, y: 0 }, { x: 0, y: 0 });
     expect(actual).toBe(true);
   });
 
-  it("LTE", () => {
+  it("LessThanOrEqual", () => {
     const actual = isLessThanOrEqual({ x: 0, y: 0 }, { x: 0, y: 1 });
     expect(actual).toBe(true);
   });
 
-  it("LT", () => {
+  it("LessThan", () => {
     const actual = isLessThan({ x: 0, y: 0 }, { x: 1, y: 1 });
     expect(actual).toBe(true);
   });
@@ -36,7 +51,7 @@ describe("public api", () => {
   });
 
   it("CNC 1 -->", () => {
-    const actual = isConcurrent({ x: 1, y: 0 }, { x: 0, y: 1 });
+    const actual = isConcurrent({ x: 1, y: 0, a: 0 }, { x: 0, y: 1, a: 0 });
     expect(actual).toBe(true);
   });
 
@@ -53,69 +68,5 @@ describe("public api", () => {
   it("CNC 2 <--", () => {
     const actual = isConcurrent({ x: 0, y: 0, z: 1 }, { x: 2, y: 2, z: 0 });
     expect(actual).toBe(true);
-  });
-});
-
-describe("clock comparison", () => {
-  it("empty clocks", () => {
-    const actual = compareClocks_internal({}, {});
-    expect(actual).toEqual([]);
-  });
-
-  it("[EQ, EQ]", () => {
-    const actual = compareClocks_internal({ x: 0, y: 0 }, { x: 0, y: 0 });
-    expect(actual).toEqual(["EQ", "EQ"]);
-  });
-
-  it("[EQ, LT]", () => {
-    const actual = compareClocks_internal({ x: 0, y: 0 }, { y: 1, x: 0 });
-    expect(actual).toEqual(["EQ", "LT"]);
-  });
-
-  it("[LT, LT]", () => {
-    const actual = compareClocks_internal({ x: 0, y: 0 }, { y: 1, x: 1 });
-    expect(actual).toEqual(["LT", "LT"]);
-  });
-
-  it("[CNC, EQ]", () => {
-    const actual = compareClocks_internal({ x: 1, y: 0 }, { y: 0, x: 0 });
-    expect(actual).toEqual(["CNC", "EQ"]);
-  });
-});
-
-describe("time ordering", () => {
-  it("both clocks empty = equal times", () => {
-    const actual = orderTimes_internal({}, {})("x");
-    expect(actual).toEqual("EQ");
-  });
-
-  it("left clock empty = less than", () => {
-    const actual = orderTimes_internal({}, { x: 0 })("x");
-    expect(actual).toEqual("LT");
-  });
-
-  it("right clock empty = concurrent", () => {
-    const actual = orderTimes_internal({ x: 0 }, {})("x");
-    expect(actual).toEqual("CNC");
-  });
-
-  it("equal times", () => {
-    const actual = orderTimes_internal({ x: 0 }, { x: 0 })("x");
-    expect(actual).toEqual("EQ");
-  });
-
-  it("equal times", () => {
-    const actual = orderTimes_internal({ x: 0 }, { x: 0 })("x");
-    expect(actual).toEqual("EQ");
-  });
-
-  it("less than time", () => {
-    const actual = orderTimes_internal({ x: 0 }, { x: 1 })("x");
-    expect(actual).toEqual("LT");
-  });
-
-  it("concurrent time", () => {
-    const actual = orderTimes_internal({ x: 1 }, { x: 0 })("x");
-    expect(actual).toEqual("CNC");
   });
 });
--- quiet-notes-web/src/crdt/clock.ts
+++ quiet-notes-web/src/crdt/clock.ts
@@ -5,64 +5,197 @@ type TimeStamp = number;
 
 type ClockOrder = LT | LTE | EQ | CNC;
 type TimeOrder = LT | EQ | CNC;
+type OrderClocks = (a: Clock, b: Clock) => ClockOrder;
 
-type LT = "LT";
-type LTE = "LTE";
-type EQ = "EQ";
-type CNC = "CNC";
+const LT = "LessThan" as const;
+const LTE = "LessThanOrEqual" as const;
+const EQ = "Equal" as const;
+const CNC = "Concurrent" as const;
+
+type LT = typeof LT;
+type LTE = typeof LTE;
+type EQ = typeof EQ;
+type CNC = typeof CNC;
+
+export type IsClockOrder = {
+  [key in `is${ClockOrder}`]: (a: Clock, b: Clock) => boolean;
+};
 
 const isClockOrder =
+  (orderClocks: OrderClocks) =>
   (order: ClockOrder) =>
   (a: Clock, b: Clock): boolean =>
-    orderClocks_internal(a, b) === order;
+    orderClocks(a, b) === order;
 
-export const isEqual = isClockOrder("EQ");
-export const isLessThan = isClockOrder("LT");
-export const isLessThanOrEqual = isClockOrder("LTE");
-export const isConcurrent = isClockOrder("CNC");
-
-export const increment = (clientId: NodeId, clock: Clock): Clock => ({
-  ...clock,
-  [clientId]: getTime(clientId, clock) + 1,
-});
-
-const orderClocks_internal = (a: Clock, b: Clock): ClockOrder => {
-  const [head, ...tail] = compareClocks_internal(a, b);
-
-  return head === undefined
-    ? "EQ"
-    : tail.reduce((result, current) => {
-        if (result === "EQ" && current === "EQ") {
-          return "EQ";
-        } else if (result === "LT" && current === "LT") {
-          return "LT";
-        } else if ((result === "EQ" || result === "LT") && (current === "EQ" || current === "LT")) {
-          return "LTE";
-        } else {
-          return "CNC";
-        }
-      }, head as ClockOrder);
+const createClockOrder = (orderClock: OrderClocks): IsClockOrder => {
+  const isOrder = isClockOrder(orderClock);
+  return {
+    isEqual: isOrder(EQ),
+    isLessThan: isOrder(LT),
+    isLessThanOrEqual: isOrder(LTE),
+    isConcurrent: isOrder(CNC),
+  };
 };
 
-export const compareClocks_internal = (a: Clock, b: Clock): TimeOrder[] => {
-  const order = orderTimes_internal(a, b);
-  const nodes = new Set([...Object.keys(a), ...Object.keys(b)]);
-  return [...nodes].map(order);
-};
+const compareClocks = (a: Clock, b: Clock): TimeOrder[] => [...nodeSet(a, b)].map(orderTimes(a, b));
 
-export const orderTimes_internal =
+const orderTimes =
   (a: Clock, b: Clock) =>
   (id: NodeId): TimeOrder => {
     const ta = getTime(id, a);
     const tb = getTime(id, b);
 
     if (ta === tb) {
-      return "EQ";
+      return EQ;
     } else if (ta < tb) {
-      return "LT";
+      return LT;
     } else {
-      return "CNC";
+      return CNC;
     }
   };
 
-const getTime = (nodeId: NodeId, clock: Clock): number => clock[nodeId] ?? Number.NEGATIVE_INFINITY;
+const getTime = (node: NodeId, clock: Clock): number => clock[node] ?? Number.NEGATIVE_INFINITY;
+
+const nodeSet = (a: Clock, b: Clock): Set<NodeId> =>
+  new Set([...Object.keys(a), ...Object.keys(b)]);
+
+export const clockOrderDeclarative = createClockOrder((a, b) => {
+  const [head, ...tail] = compareClocks(a, b);
+
+  return head === undefined
+    ? EQ
+    : tail.reduce((result, current) => {
+        if (result === EQ && current === EQ) {
+          return EQ;
+        } else if (result === LT && current === LT) {
+          return LT;
+        } else if ((result === EQ || result === LT) && (current === EQ || current === LT)) {
+          return LTE;
+        } else {
+          return CNC;
+        }
+      }, head as ClockOrder);
+});
+
+export const clockOrderImperative = createClockOrder((a, b) => {
+  let result: ClockOrder | undefined;
+
+  for (let node of nodeSet(a, b)) {
+    const current = orderTimes(a, b)(node);
+
+    if (result === undefined) {
+      result = current;
+    } else if (result === EQ && current === EQ) {
+      result = EQ;
+    } else if (result === LT && current === LT) {
+      result = LT;
+    } else if ((result === EQ || result === LT) && (current === EQ || current === LT)) {
+      result = LTE;
+    } else {
+      result = CNC;
+      break;
+    }
+  }
+
+  return result ?? EQ;
+});
+
+export const clockOrderImperativeInlined = createClockOrder((a, b) => {
+  let result: ClockOrder | undefined;
+
+  for (let node of nodeSet(a, b)) {
+    let current: TimeOrder;
+
+    const ta = a[node] ?? Number.NEGATIVE_INFINITY;
+    const tb = b[node] ?? Number.NEGATIVE_INFINITY;
+
+    if (ta === tb) {
+      current = EQ;
+    } else if (ta < tb) {
+      current = LT;
+    } else {
+      current = CNC;
+    }
+
+    if (result === undefined) {
+      result = current;
+    } else if (result === EQ && current === EQ) {
+      result = EQ;
+    } else if (result === LT && current === LT) {
+      result = LT;
+    } else if ((result === EQ || result === LT) && (current === EQ || current === LT)) {
+      result = LTE;
+    } else {
+      result = CNC;
+      break;
+    }
+  }
+
+  return result ?? EQ;
+});
+
+export const clockOrderImperativeForEach = createClockOrder((a, b) => {
+  let result: ClockOrder | undefined;
+
+  nodeSet(a, b).forEach((node) => {
+    const current = orderTimes(a, b)(node);
+
+    if (result === undefined) {
+      result = current;
+    } else if (result === EQ && current === EQ) {
+      result = EQ;
+    } else if (result === LT && current === LT) {
+      result = LT;
+    } else if ((result === EQ || result === LT) && (current === EQ || current === LT)) {
+      result = LTE;
+    } else {
+      result = CNC;
+    }
+  });
+
+  return result ?? EQ;
+});
+
+export const clockOrderZ = createClockOrder((a, b) => {
+  const nodes = [...Object.keys(a), ...Object.keys(b)];
+  const seen: Record<NodeId, boolean> = {};
+  let result: ClockOrder | undefined;
+
+  for (let i = 0; i < nodes.length; i++) {
+    const node = nodes[i];
+
+    if (node === undefined || seen[node]) {
+      continue;
+    }
+
+    seen[node] = true;
+
+    let current: TimeOrder;
+
+    const ta = a[node] ?? Number.NEGATIVE_INFINITY;
+    const tb = b[node] ?? Number.NEGATIVE_INFINITY;
+
+    if (ta === tb) {
+      current = EQ;
+    } else if (ta < tb) {
+      current = LT;
+    } else {
+      current = CNC;
+    }
+
+    if (result === undefined) {
+      result = current;
+    } else if (result === EQ && current === EQ) {
+      result = EQ;
+    } else if (result === LT && current === LT) {
+      result = LT;
+    } else if ((result === EQ || result === LT) && (current === EQ || current === LT)) {
+      result = LTE;
+    } else {
+      result = CNC;
+      break;
+    }
+  }
+
+  return result ?? EQ;
+});
