a1478b348260319baf3a1de9af272cd7eb20fcd6
Leon Coto <leoncoto@gmail.com>
2022-04-05 23:15:37 +0100

    Implement vector clock algorithm

--- quiet-notes-web/src/crdt/clock.test.ts
+++ quiet-notes-web/src/crdt/clock.test.ts
@@ -1,4 +1,13 @@
-import { isConcurrent, isEqual, isLessThan, isLessThanOrEqual } from "./clock";
+import {
+  merge,
+  increment,
+  initialize,
+  isConcurrent,
+  isEqual,
+  isLessThan,
+  isLessThanOrEqual,
+  receive,
+} from "./clock";
 
 describe("Clock Order", () => {
   it("empty clocks", () => {
@@ -56,5 +65,47 @@ describe("Clock Order", () => {
   it("CNC 2 <--", () => {
     const actual = isConcurrent({ x: 0, y: 0, z: 1 }, { x: 2, y: 2, z: 0 });
     expect(actual).toBe(true);
+  });
+});
+
+describe("vector clock algorithm", () => {
+  it("initialize", () => {
+    const actual = initialize("a");
+    expect(actual).toEqual({ a: 0 });
+  });
+
+  it("increment: when node is not in the clock", () => {
+    const actual = increment("x", {});
+    expect(actual).toEqual({});
+  });
+
+  it("increment: when node is in the clock", () => {
+    const actual = increment("x", { x: 0, y: 0 });
+    expect(actual).toEqual({ x: 1, y: 0 });
+  });
+
+  it("merge two empty clocks", () => {
+    const actual = merge({}, {});
+    expect(actual).toEqual({});
+  });
+
+  it("merge empty to non empty", () => {
+    const actual = merge({}, { x: 0 });
+    expect(actual).toEqual({ x: 0 });
+  });
+
+  it("merge non empty to empty", () => {
+    const actual = merge({ x: 0 }, {});
+    expect(actual).toEqual({ x: 0 });
+  });
+
+  it("merge non empty to non empty", () => {
+    const actual = merge({ x: 0, y: 1, z: 0, a: 0 }, { x: 0, y: 0, z: 1 });
+    expect(actual).toEqual({ x: 0, y: 1, z: 1, a: 0 });
+  });
+
+  it("receive", () => {
+    const actual = receive("x", { x: 0 }, { x: 1, y: 0 });
+    expect(actual).toEqual({ x: 2, y: 0 });
   });
 });
--- quiet-notes-web/src/crdt/clock.ts
+++ quiet-notes-web/src/crdt/clock.ts
@@ -19,7 +19,7 @@ type TimeStamp = number;
 const orderClocks = (a: Clock, b: Clock): ClockOrder => {
   let result: ClockOrder | undefined;
 
-  for (let n of new Set(Object.keys(a).concat(Object.keys(b)))) {
+  for (let n of nodeSet(a, b)) {
     const order = orderNodes(a, b, n);
 
     if (result === undefined) {
@@ -53,6 +53,7 @@ const orderNodes = (a: Clock, b: Clock, n: NodeId): NodeOrder => {
 };
 
 const getTime = (node: NodeId, clock: Clock): number => clock[node] ?? Number.NEGATIVE_INFINITY;
+const nodeSet = (a: Clock, b: Clock): Set<NodeId> => new Set(Object.keys(a).concat(Object.keys(b)));
 
 const isClockOrder =
   (o: ClockOrder) =>
@@ -63,3 +64,21 @@ export const isEqual = isClockOrder(EQ);
 export const isLessThan = isClockOrder(LT);
 export const isLessThanOrEqual = isClockOrder(LTE);
 export const isConcurrent = isClockOrder(CNC);
+
+export const initialize = (nodeId: NodeId): Clock => ({ [nodeId]: 0 });
+
+export const increment = (nodeId: NodeId, clock: Clock) => {
+  const time = clock[nodeId];
+  return time === undefined ? clock : { ...clock, [nodeId]: time + 1 };
+};
+
+export const merge = (a: Clock, b: Clock): Clock => {
+  const clock: Clock = {};
+  for (let n of nodeSet(a, b)) {
+    clock[n] = Math.max(getTime(n, a), getTime(n, b));
+  }
+  return clock;
+};
+
+export const receive = (nodeId: NodeId, clock: Clock, incoming: Clock): Clock =>
+  increment(nodeId, merge(clock, incoming));
