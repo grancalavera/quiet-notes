2c9959b706bbc85ec1c241144822360c40f8658f
Leon Coto <leoncoto@gmail.com>
2022-10-15 18:09:39 +0100

    Fix for new version of notes on load

--- quiet-notes-web/src/note/note-editor.tsx
+++ quiet-notes-web/src/note/note-editor.tsx
@@ -2,15 +2,17 @@ import { TextareaAutosize } from "@mui/material";
 import Box from "@mui/material/Box";
 import { Subscribe } from "@react-rxjs/core";
 import { useEffect, useRef, VFC } from "react";
-import { Redirect, useLocation } from "react-router";
-import { useErrorHandler } from "../app/app-error-state";
+import { Redirect } from "react-router";
 import { LoadingLayout } from "../layout/loading-layout";
-import { isLoadFailure } from "../lib/load-result";
 import { useSelectedNoteId } from "../notebook/notebook-state";
-import { saveNote, updateNote, useNoteById, useSaveNoteResult } from "./note-state";
+import { setOpenNoteId, updateNote, useNoteById } from "./note-state";
 
 export const NoteEditor = () => {
   const noteId = useSelectedNoteId();
+
+  useEffect(() => {
+    setOpenNoteId(noteId);
+  }, [noteId]);
 
   return noteId ? (
     <Subscribe fallback={<LoadingLayout />} key={noteId}>
@@ -22,20 +24,6 @@ export const NoteEditor = () => {
 const NoteEditorInternal: VFC<{ noteId: string }> = ({ noteId }) => {
   const note = useNoteById(noteId);
   const inputRef = useRef<HTMLTextAreaElement | null>(null);
-  const result = useSaveNoteResult();
-  const handleError = useErrorHandler();
-
-  useEffect(() => {
-    isLoadFailure(result) && handleError(result.error);
-  }, [result]);
-
-  useEffect(() => {
-    if (note !== undefined) {
-      saveNote(note);
-    } else {
-      console.log("should navigate away");
-    }
-  }, [note]);
 
   return note === undefined ? (
     <Redirect to="/notebook" />
--- quiet-notes-web/src/notebook-toolbars/notebook-editor-toolbar.tsx
+++ quiet-notes-web/src/notebook-toolbars/notebook-editor-toolbar.tsx
@@ -2,12 +2,10 @@ import ButtonGroup from "@mui/material/ButtonGroup";
 import { CreateNoteButton } from "./create-note-button";
 import { DeleteNoteButton } from "./delete-note-button";
 import { NotebookToolbarLayout } from "./notebook-toolbar-layout";
-import { UpdateMonitor } from "./update-monitor";
 
 export const NotebookEditorToolbar = () => {
   return (
     <NotebookToolbarLayout>
-      <UpdateMonitor />
       <ButtonGroup>
         <DeleteNoteButton />
         <CreateNoteButton />
