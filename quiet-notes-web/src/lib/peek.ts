15ca4756c31f5c98a8731bcd53021ad51d6002eb
Leon Coto <leoncoto@gmail.com>
2022-10-26 21:59:49 +0100

    fix note synchronization problems

--- quiet-notes-web/src/lib/peek.ts
+++ quiet-notes-web/src/lib/peek.ts
@@ -5,20 +5,5 @@ type Transform<T> = (x: T) => unknown;
 
 export const peek = <T>(label: string, f: Transform<T> = (x) => x): MonoTypeOperatorFunction<T> =>
   tap<T>((data) => {
-    console.log(`[${label}]`, { data: f(data) });
-  });
-
-export const peekStart = <T>(
-  groupName: string,
-  f: Transform<T> = (x) => x
-): MonoTypeOperatorFunction<T> =>
-  tap<T>((data) => {
-    console.group(`[${groupName}]`);
-    console.log("[enter]", { data: f(data) });
-  });
-
-export const peekEnd = <T>(f: Transform<T> = (x) => x): MonoTypeOperatorFunction<T> =>
-  tap<T>((data) => {
-    console.log("[exit]", { data: f(data) });
-    console.groupEnd();
+    console.log(`${label}`, { data: f(data) });
   });
