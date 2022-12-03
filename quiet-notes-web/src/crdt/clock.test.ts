d0e620d3c2a80d0fd83ab47494e257d8c59befc8
Leon Coto <leoncoto@gmail.com>
2022-12-03 16:01:09 +0000

    fix note concurrency issues

--- quiet-notes-web/src/crdt/clock.test.ts
+++ quiet-notes-web/src/crdt/clock.test.ts
@@ -1,18 +1,9 @@
-import {
-  merge,
-  increment,
-  initialize,
-  isConcurrent,
-  isEqual,
-  isLessThan,
-  isLessThanOrEqual,
-  receive,
-} from "./clock";
+import { merge, increment, initialize, isLessThan, receive } from "./clock";
 
 describe("Clock Order", () => {
   it("empty clocks", () => {
-    const actual = isEqual({}, {});
-    expect(actual).toBe(true);
+    const actual = isLessThan({}, {});
+    expect(actual).toBe(false);
   });
 
   it("right empty", () => {
@@ -22,48 +13,11 @@ describe("Clock Order", () => {
 
   it("left empty", () => {
     const actual = isLessThan({}, { x: 0 });
-    expect(actual).toBe(true);
-  });
-
-  it("Equal", () => {
-    const actual = isEqual({ x: 0, y: 0 }, { x: 0, y: 0 });
-    expect(actual).toBe(true);
-  });
-
-  it("LessThanOrEqual", () => {
-    const actual = isLessThanOrEqual({ x: 0, y: 0 }, { x: 0, y: 1 });
-    expect(actual).toBe(true);
+    expect(actual).toBe(false);
   });
 
   it("LessThan", () => {
     const actual = isLessThan({ x: 0, y: 0 }, { x: 1, y: 1 });
-    expect(actual).toBe(true);
-  });
-
-  it("LT then CNC", () => {
-    const a = { x: 0, y: 0 };
-    const b = { x: 1, y: 1 };
-    expect(isLessThan(a, b)).toBe(true);
-    expect(isConcurrent(b, a)).toBe(true);
-  });
-
-  it("CNC 1 -->", () => {
-    const actual = isConcurrent({ x: 1, y: 0, a: 0 }, { x: 0, y: 1, a: 0 });
-    expect(actual).toBe(true);
-  });
-
-  it("CNC 1 <--", () => {
-    const actual = isConcurrent({ x: 0, y: 1 }, { x: 1, y: 0 });
-    expect(actual).toBe(true);
-  });
-
-  it("CNC 2 -->", () => {
-    const actual = isConcurrent({ x: 2, y: 2, z: 0 }, { x: 0, y: 0, z: 1 });
-    expect(actual).toBe(true);
-  });
-
-  it("CNC 2 <--", () => {
-    const actual = isConcurrent({ x: 0, y: 0, z: 1 }, { x: 2, y: 2, z: 0 });
     expect(actual).toBe(true);
   });
 });
