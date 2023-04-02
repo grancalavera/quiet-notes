a66fb1d49ea675ebd527e3722626f099d9dd635f
Leon Coto <leoncoto@gmail.com>
2023-04-02 20:14:24 +0100

    fix editor styles so that they work with the theme

--- quiet-notes-web/src/note/note-editor.tsx
+++ quiet-notes-web/src/note/note-editor.tsx
@@ -1,5 +1,4 @@
-import { Stack, TextareaAutosize } from "@mui/material";
-import Box from "@mui/material/Box";
+import { Stack, styled } from "@mui/material";
 import { Subscribe } from "@react-rxjs/core";
 import { useEffect, useRef } from "react";
 import { Loading } from "../components/loading";
@@ -13,10 +12,10 @@ import { updateNote, useNote } from "./note-state";
 
 export const NoteEditor = () => {
   return (
-    <Stack direction={"row"} width="100%" height="100%" gap={1} padding={1}>
+    <StyleNoteEditor direction={"row"}>
       <MainNoteEditor />
       <AdditionalNoteEditor />
-    </Stack>
+    </StyleNoteEditor>
   );
 };
 
@@ -58,27 +57,46 @@ const NoteEditorInternal = ({ noteId }: { noteId: string }) => {
   }, []);
 
   return note ? (
-    <Box
-      sx={{
-        overflow: "hidden",
-        height: "100%",
-      }}
-    >
-      <TextareaAutosize
+    <Stack height="100%" width="100%">
+      <Spacer />
+      <StyledTextArea
         aria-label="a quiet note"
         ref={inputRef}
         value={note.content}
         onChange={(e) => {
           updateNote({ ...note, content: e.target.value });
         }}
-        style={{
-          resize: "none",
-          width: "100%",
-          height: "100%",
-          overflow: "auto",
-          padding: "1em",
-        }}
       />
-    </Box>
+      <Spacer />
+    </Stack>
   ) : null;
 };
+
+const StyleNoteEditor = styled(Stack)`
+  height: 100%;
+  width: 100%;
+  gap: ${(p) => p.theme.spacing(1)};
+  padding: ${(p) => p.theme.spacing(1)};
+  background-color: ${(p) =>
+    p.theme.palette.mode === "dark"
+      ? p.theme.palette.grey[900]
+      : p.theme.palette.grey[200]};
+`;
+
+const StyledTextArea = styled("textarea")`
+  resize: none;
+  width: 100%;
+  height: 100%;
+  border: none;
+  padding: 0 ${(p) => p.theme.spacing(2)};
+  color: ${(p) => p.theme.palette.text.primary};
+  background-color: ${(p) => p.theme.palette.background.paper};
+  outline: none;
+  padding-bottom: 100px;
+  flex-grow: 1;
+`;
+
+const Spacer = styled("div")`
+  height: ${(p) => p.theme.spacing(2)};
+  background-color: ${(p) => p.theme.palette.background.paper};
+`;
