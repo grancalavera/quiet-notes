c739983ca68c8c5c7caae3f473725e627ec5d3b2
Leon Coto <leoncoto@gmail.com>
2023-05-13 14:52:32 +0100

    filter out nullable users from admin service

--- quiet-notes-web/src/firebase/admin-service.ts
+++ quiet-notes-web/src/firebase/admin-service.ts
@@ -2,7 +2,7 @@ import { collection } from "firebase/firestore";
 import { httpsCallable } from "firebase/functions";
 import { QNToggleRole } from "quiet-notes-lib";
 import { collectionData } from "rxfire/firestore";
-import { firstValueFrom, switchMap } from "rxjs";
+import { firstValueFrom, map, switchMap } from "rxjs";
 import { AdminServiceSchema } from "../admin/admin-service-schema";
 import { userConverter } from "./auth-service-model";
 import { firestore$, functions$ } from "./firebase";
@@ -16,7 +16,8 @@ const users$ = firestore$.pipe(
   switchMap((firestore) => {
     const ref = collection(firestore, "users").withConverter(userConverter);
     return collectionData(ref);
-  })
+  }),
+  map(removeNullables)
 );
 
 export const adminService: AdminServiceSchema = {
---
+++ quiet-notes-web/src/lib/isNotNullable.ts
@@ -0,0 +1,22 @@
+/**
+ * Checks if a value is not null or undefined.
+ *
+ * This function is useful for when you want to check that a value is
+ * not null or undefined, but you don't want to go through the hassle
+ * of writing out the whole `value !== null && value !== undefined`
+ * expression.
+ *
+ * @param value The value to check.
+ * @returns `true` if the value is not null or undefined, `false`
+ * otherwise.
+ */
+const isNotNullable = <T>(value: T | null | undefined): value is T =>
+  value !== null && value !== undefined;
+
+/**
+ * Remove null and undefined values from an array.
+ *
+ * @param xs - The array to remove null and undefined values from.
+ */
+const removeNullables = <T>(xs: (T | null | undefined)[]): T[] =>
+  xs.filter(isNotNullable);
