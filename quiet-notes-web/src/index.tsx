fa38bd542bac361fb224e26d35d1717057125fa5
Leon Coto <grancalavera@users.noreply.github.com>
2024-08-17 12:34:10 +0100

    Udpate deps (#75)
    * update minor and patch dependencies
    
    * update major deps
    
    * update gha to use pnpm v9

--- quiet-notes-web/src/index.tsx
+++ quiet-notes-web/src/index.tsx
@@ -7,7 +7,6 @@ import "normalize.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Application } from "./app/application";
-import reportWebVitals from "./reportWebVitals";
 
 const root = createRoot(document.getElementById("root")!);
 
@@ -16,8 +15,3 @@ root.render(
     <Application />
   </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
